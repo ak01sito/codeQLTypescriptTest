bd56882b91963f1b8025c96c85b9ccbe531eab13
ak01sito <canomire@gmail.com>
2023-09-26 00:11:47 +0200

    initial vulnerable code

---
+++ xss-vulnerable-code.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/search', (req, res) => {
+  const searchTerm = req.query.q; // Simulating user input
+
+  // Vulnerable code: rendering user input directly in the response
+  const result = `<p>You searched for: ${searchTerm}</p>`;
+  
+  res.send(result);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
